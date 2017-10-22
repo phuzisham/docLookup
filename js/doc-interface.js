c93c5fb6e76d8258a5785164a7a4505b180aa0ad
David Hale <david.hale7@gmail.com>
2017-10-22 14:36:35 -0700

    fixed minor bugs

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -5,45 +5,38 @@ $(document).ready(function(){
   $('#search').submit(function(e) {
     e.preventDefault();
     let query = $('#symptom').val();
-    displayDoctors(query);
+    displayDoctorsSymptom(query);
   });
 
-
+  $('#searchName').submit(function(e) {
+    e.preventDefault();
+    let name = $('#name').val();
+    displayDoctorsName(name);
+  });
 });
 
-function displayDoctors(query) {
-  docClass.callApi(query).then(function(response) {
+function displayDoctorsSymptom(query) {
+  docClass.callApiSymptom(query).then(function(response) {
     let parsed = JSON.parse(response);
     let arr = [];
     parsed.data.forEach(function(doc) {
-      debugger;
       arr.push(doc.practices);
     });
     arr.forEach(function(doc) {
-      // debugger;
       $('#docName').append(`<li><a href='${doc[0].website}' id='${doc.uid}' class='doc'>${doc[0].name}</a></li>`);
     });
-    // displayDoctors();
   });
 }
 
-// function displayDoctors() {
-//   $('a').click(function(event) {
-//     event.preventDefault();
-//     let manf = event.currentTarget.id;
-//     console.log(manf);
-//     bikeClass.manufacturerApi(manf).then(function(response) {
-//       let parsed = JSON.parse(response);
-//       let arr = [];
-//       parsed.bikes.forEach(function(bike) {
-//         arr.push(bike);
-//       });
-//       arr.forEach(function(bike) {
-//         $('#manStole').append(`<li class="details" id=${bike.id}><a>${bike.title}</a></li>`);
-//       });
-//       $('#manName').hide(800);
-//       $('#manStole').show(800);
-//       manufacturerDetails();
-//     });
-//   });
-// }
+function displayDoctorsName(name) {
+  docClass.callApiName(name).then(function(response) {
+    let parsed = JSON.parse(response);
+    let arr = [];
+    parsed.data.forEach(function(doc) {
+      arr.push(doc.practices);
+    });
+    arr.forEach(function(doc) {
+      $('#docName').append(`<li><a href='${doc[0].website}' id='${doc.uid}' class='doc'>${doc[0].name}</a></li>`);
+    });
+  });
+}
--- js/doc.js
+++ js/doc.js
@@ -1,8 +1,7 @@
+const apiKey = require('./../.env').apiKey;
 export class Doc {
+  callApiSymptom(query) {
 
-
-  callApi(query) {
-    let apiKey = require('./../.env').apiKey;
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=45.521728%2C-122.67326%2C20&skip=0&limit=10&user_key=${apiKey}`;
@@ -18,21 +17,20 @@ export class Doc {
     });
   }
 
-  // manufacturerApi(manf) {
-  //   return new Promise(function(resolve, reject) {
-  //     let request = new XMLHttpRequest();
-  //     let url = `https://bikeindex.org:443/api/v3/search?page=1&per_page=200&manufacturer=${manf}&location=45.521728%2C-122.67326&distance=10&stolenness=proximity`;
-  //     console.log(url);
-  //
-  //     request.onload = function() {
-  //       if (this.status === 200) {
-  //         resolve(request.response);
-  //       } else {
-  //         reject(Error(request.statusText));
-  //       }
-  //     };
-  //     request.open("GET", url, true);
-  //     request.send();
-  //   });
-  // }
+  callApiName(name) {
+    // const apiKey = require('./../.env').apiKey;
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.521728%2C-122.67326%2C20&skip=0&limit=10&user_key=${apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
 }
