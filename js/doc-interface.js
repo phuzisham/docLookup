0b81d3467ed44cfe95ed092e8010412e624be4b0
David Hale <david.hale7@gmail.com>
2017-10-22 11:10:25 -0700

    display names

--- js/doc-interface.js
+++ js/doc-interface.js
@@ -4,10 +4,11 @@ let docClass = new Doc();
 $(document).ready(function(){
   $('#search').submit(function(e) {
     e.preventDefault();
-    alert('djf');
     let query = $('#symptom').val();
     displayDoctors(query);
-  })
+  });
+
+
 });
 
 function displayDoctors(query) {
@@ -15,10 +16,12 @@ function displayDoctors(query) {
     let parsed = JSON.parse(response);
     let arr = [];
     parsed.data.forEach(function(doc) {
+      debugger;
       arr.push(doc.practices);
     });
     arr.forEach(function(doc) {
-      $('#docName').append(`<li><a href='${doc[0].website}' id='${doc}' class='doc'>${doc[0].name}</a></li>`);
+      // debugger;
+      $('#docName').append(`<li><a href='${doc[0].website}' id='${doc.uid}' class='doc'>${doc[0].name}</a></li>`);
     });
     // displayDoctors();
   });
--- js/doc.js
+++ js/doc.js
@@ -6,8 +6,6 @@ export class Doc {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=45.521728%2C-122.67326%2C20&skip=0&limit=10&user_key=${apiKey}`;
-      // debugger;
-
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
